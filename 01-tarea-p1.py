9c1ec856edd7106e6cbfa0219b90d3275d3831d2
FrancoClaverie <franco_claverierojas@hotmail.com>
2018-09-27 02:45:42 -0300

    P2 parte 2
    Se termino la parte 2 de la P2, con refinamiento incluido

--- 01-tarea-p1.py
+++ 01-tarea-p1.py
@@ -2,14 +2,10 @@
 
 #Mi RUT: 19.687.559-K
 
-#Se pide comparar la precision de 2 metodos de
-#derivacion discreta, uno de error de orden 4 (O(h^4))
-#descrito en el enunciado y otro de orden 1 (O(h)) ensenado
-#en clases. Esto para f(x) = -cos(x); f'(x) = sin(x), para
-#x = 1.XXX, donde XXX son los 3 ultimos numeros de mi RUT
-#antes del digito verificador (XXX = 559). Primero se 
-#realizara la comparacion usando numeros float32 y luego
-#float64 (este computador no puede usar float128), 
+#Se pide comparar la precision de 2 metodos de derivacion discreta, uno de error de orden 4 (O(h^4))
+#descrito en el enunciado y otro de orden 1 (O(h)) ensenado en clases. Esto para f(x) = -cos(x); f'(x) = sin(x), para
+#x = 1.XXX, donde XXX son los 3 ultimos numeros de mi RUT antes del digito verificador (XXX = 559). Primero se 
+#realizara la comparacion usando numeros float32 y luego float64 (este computador no puede usar float128), 
 #comparando tambien el resultados entre ellos.
 
 #f'(x) =(-f(x+2*h)+8*f(x+h)-8*f(x-h)+f(x-2*h))/(12*h) + O(h^4)
--- 01-tarea-p2.py
+++ 01-tarea-p2.py
@@ -5,6 +5,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import matplotlib as mp 
+import math as m
+from astropy import constants as astro   #modulo con las constantes que se usan en la funcion de Planck
 
 #Parte 1
 
@@ -24,11 +26,12 @@ archivo = np.loadtxt('firas_monopole_spec_v1.txt')
 #De estas columnas nos interesan las columnas 1 (frecuencia), 2 (esectro medido) y 4 (incertidumbre/error) para la
 #realizacion del grafico.
 
-frecuencia = archivo[:,0]  #primera columna, es de tipo np.ndarray
-espectro = archivo[:,1]    #segunda columna
-error = archivo[:,3]/1000  #cuarta columna
+frecuencia = archivo[:,0]*100*astro.c.value          #primera columna, es de tipo np.ndarray
+espectro = archivo[:,1]                              #segunda columna
+error = archivo[:,3]/1000                            #cuarta columna
 
-#Se tiene que el espectro medido esta en [MJy/sr] y el error en [kJy/sr], por lo que se divide el error por mil
+#Se tiene que el espectro medido esta en [MJy/sr] y el error en [kJy/sr], por lo que se divide el error por mil, tambien la frecuencia
+#esta en [cm^-1], por lo que se ultiplica por 100 y por c para quedar en [1/s = Hz]
 
 #Como el error es muy pequeño por la precision del FIRAS, se multiplica por 400 (factor sugerido como ejemplo en el enunciado)
 
@@ -38,10 +41,74 @@ error_400 = 400*error
 """
 plt.plot(frecuencia, espectro)
 plt.title('Espectro de monopolo medido por FIRAS')
-plt.xlabel('Frecuencia [$cm^{-1}$]')
+plt.xlabel('Frecuencia [Hz]')
 plt.ylabel('Espectro del monopolo [MJy/sr]')
 plt.errorbar(frecuencia, espectro, xerr=0, yerr=error_400)
 
 plt.savefig('espectro_monopolo_1.png')
 plt.show()
 """
+
+#Parte 2
+
+#P = *integral_{0}^{infinito}(x^3/(exp(x)-1))dx = K(T)*I
+
+integral_analitica = (np.pi**4)/15 
+
+#Para resolver la integral, se utiliza el cambio de variable sugerido y = arctan(x) y asi poder cambiar lo limites de integracion
+
+#y = arctan(x); x = tan(y)
+#dy = (1/(1+x^2))dx; dx = (1+x^2)dy
+
+#x = 0 entonces y = arctan(0) = 0
+#x = infinito entonces lim_{x = infinito}arctan(x) = pi/2
+
+#P = K(T)*integral_{0}^{pi/2}(tan^3(y))(1+tan^2(y))/(exp(tan(y)) - 1)dy = K(T)*integral_{0}^{pi/2}f(y)dy
+
+#Se tiene que f(y) tiene numerador y denominador infinitos en y = 0 y solo denominador infinito en en y = pi/2, por lo que
+#se usara el metodo del valor medio en ambos extremos. Se resolvera con el metodo de Simpson 1/3 para lo demas
+
+#Definimos la función dentro de la integral
+
+def f_integral(x):
+    out = (np.tan(x)**3 + np.tan(x)**5)/(np.exp(np.tan(x)) - 1)
+    return out
+
+#Para calcular con dicho metodo, se debe de escoger un h tal que la integral se acerque a su valor analitico, por lo que
+#se refinarra el valor de la integral al ir aumentando al doble el numero de terminos que se dividira (b - a) = (pi/2 - 0) = pi/2
+
+#Sabemos que la integral tiene un valor analitico igual a (np.pi**4)/15, por lo que se generara un particion cada vez 
+# la mitad de tamaño que la anterior hasta estar cerca de ese valor. Se debe definir entonces una tolerancia
+
+x_0 = 0
+x_n = np.pi/2
+
+
+def valor_integral(a,b, N=10 , toler=1e-10):
+    integral = 0
+    while np.abs(integral - integral_analitica) > toler:
+        integral = 0
+        h = (b-a)/N
+        for i in range(N+1):
+            x = a + h*i    #valor a evaluar la funcion f_integral
+            if x == a:
+                integral += h*f_integral(x + h/2)   #valor medio en el inicio 
+            elif x == b:
+                integral += h*f_integral(x - h/2)   #valor medio en el final 
+            elif i % 2 == 0:   #si el numero de termino es par
+                integral += 2*f_integral(x)
+            else: #si el numero de termino es impar y no se esta evaluando en los extremos
+                integral += 4*f_integral(x)
+        integral += f_integral(a+h) + f_integral(b-h)  #se le suman los terminos inicial y final del metodo de Simpson 1/3
+        integral *= h/3        
+        N *= 2  #se duplica el numero de divisiones, disminuye a la mitad h
+    print('N =', N)
+    return integral
+
+
+
+
+
+
+
+
