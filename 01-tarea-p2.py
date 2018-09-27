f11c720ce606788956ee194af52fd1bb65059919
FrancoClaverie <franco_claverierojas@hotmail.com>
2018-09-26 21:44:21 -0300

    P2 parte 1
    Se acaba de hacer el gráfico de la parte 1 de la P2

--- 01-tarea-p2.py
+++ 01-tarea-p2.py
@@ -3,7 +3,7 @@
 #Primero que nada, se importan los modulos a utilizar
 
 import numpy as np 
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import matplotlib as mp 
 
 #Parte 1
@@ -15,7 +15,7 @@ archivo = np.loadtxt('firas_monopole_spec_v1.txt')
 
 #Del archivo, se tiene que cada columna significa:
 
-#Columna 1: Frecuencia [Hz]
+#Columna 1: Frecuencia [cm^-1]
 #Columna 2: Espectro del monopolo medido por el instrumento FIRAS del satelite COBE [MJy/sr], donde 1 [MJy] = 10^−20 [Wm^−2Hz^−1] 
 #Columna 3: Espectro residual del monopolo [kJy/sr]
 #Columna 4: Incertidumbre del espectro medido [kJy/sr]
@@ -24,3 +24,24 @@ archivo = np.loadtxt('firas_monopole_spec_v1.txt')
 #De estas columnas nos interesan las columnas 1 (frecuencia), 2 (esectro medido) y 4 (incertidumbre/error) para la
 #realizacion del grafico.
 
+frecuencia = archivo[:,0]  #primera columna, es de tipo np.ndarray
+espectro = archivo[:,1]    #segunda columna
+error = archivo[:,3]/1000  #cuarta columna
+
+#Se tiene que el espectro medido esta en [MJy/sr] y el error en [kJy/sr], por lo que se divide el error por mil
+
+#Como el error es muy pequeño por la precision del FIRAS, se multiplica por 400 (factor sugerido como ejemplo en el enunciado)
+
+error_400 = 400*error
+
+#Grafico:
+"""
+plt.plot(frecuencia, espectro)
+plt.title('Espectro de monopolo medido por FIRAS')
+plt.xlabel('Frecuencia [$cm^{-1}$]')
+plt.ylabel('Espectro del monopolo [MJy/sr]')
+plt.errorbar(frecuencia, espectro, xerr=0, yerr=error_400)
+
+plt.savefig('espectro_monopolo_1.png')
+plt.show()
+"""
